dfb4dfd2eecedc6f121ff8635f5336f215c2da8d
FazleRabbiBindu <fazlerabbibindu@gmail.com>
2024-02-19 17:30:33 +0600

    coupone code section working good

--- assets/styles/scripts.js
+++ assets/styles/scripts.js
@@ -2,7 +2,7 @@ var availableSeats = document.getElementById('availableSeat');
 var seatCounter = document.getElementById('seatCount');
 var totalPriceId = document.getElementById('totalPrice');
 var grandTotalId = document.getElementById('grandTotal');
-var numberValue =document.getElementById('phoneNumber').value;
+var numberValue = document.getElementById('phoneNumber').value;
 
 var totalSeat = 8;
 var totalPrice = 0;
@@ -10,45 +10,41 @@ var discount = 0;
 var selectedSeat = 0;
 var selectedSeatNumber = [];
 
-function fairCalculation(seatNumber)
-{
+function fairCalculation(seatNumber) {
     selectedSeatNumber[selectedSeat] = seatNumber;
     selectedSeat += 1;
     totalPrice += 550;
 
-    totalPriceId.innerText=totalPrice
+    totalPriceId.innerText = totalPrice
     seatCounter.innerText = selectedSeat;
     grandTotalId.innerText = totalPrice;
-    if(selectedSeat==4)
-    {
+    if (selectedSeat == 4) {
         document.getElementById('couponApply').classList.remove('btn-disabled')
     }
 
 }
-function elementManipulation(seatNumber)
-{
-    numberValue =parseInt(document.getElementById('phoneNumber').value);
-    
-    if(selectedSeatNumber.length>0 && !isNaN(numberValue)){
+function elementManipulation(seatNumber) {
+    numberValue = parseInt(document.getElementById('phoneNumber').value);
+
+    if (selectedSeatNumber.length > 0 && !isNaN(numberValue)) {
         document.getElementById('nextButton').classList.remove('btn-disabled');
     }
-    else{
+    else {
         document.getElementById('nextButton').classList.add('btn-disabled');
     }
     // document.getElementById(seatNumber).classList.add('btn-disabled');
-    document.getElementById(seatNumber).classList.replace( 'bg-[#F7F8F8]', 'bg-[#1DD100]');
+    document.getElementById(seatNumber).classList.replace('bg-[#F7F8F8]', 'bg-[#1DD100]');
 
-    document.getElementById('book'+seatNumber).classList.remove('hidden');
+    document.getElementById('book' + seatNumber).classList.remove('hidden');
 }
 function seatSelection(seatNumber) {
-    if(!selectedSeatNumber.includes(seatNumber))
-    {
+    if (!selectedSeatNumber.includes(seatNumber)) {
         totalSeat = totalSeat - 1;
         availableSeats.innerText = totalSeat;
         fairCalculation(seatNumber);
         elementManipulation(seatNumber);
     }
-    else{
+    else {
         unhideEl('seatSelected');
     }
 }
@@ -62,65 +58,81 @@ function bookSeat(seatNumber) {
 
 }
 
-function unhideEl(popIndex){
-    switch (popIndex){
+function unhideEl(popIndex) {
+    switch (popIndex) {
         case 'lowSeat':
             document.getElementById('lowSeat').classList.remove('hidden');
-        break;
+            break;
         case 'notPhone':
             document.getElementById('notPhone').classList.remove('hidden');
-        break;
+            break;
+        case 'couponCode':
+            document.getElementById('couponCodeAlert').classList.remove('hidden');
+            break;
         default:
 
     }
 }
-function hideEl(popIndex){
-    switch (popIndex){
+function hideEl(popIndex) {
+    switch (popIndex) {
         case 'lowSeat':
             document.getElementById('lowSeat').classList.add('hidden');
-        break;
+            break;
         case 'notPhone':
             document.getElementById('notPhone').classList.add('hidden');
-        break;
+            break;
+        case 'couponCode':
+            document.getElementById('couponCodeAlert').classList.add('hidden');
+            break;
         default:
 
     }
 }
 
-function next()
-{
-    seatsBooked = selectedSeatNumber.length;
-    // console.log(seatsBooked);
-    
+function next() {
     element = document.getElementById('my_modal_1');
     phone = document.getElementById('phoneNumber');
-    var phoneNumber = parseInt(phone.value);
-    console.log(phoneNumber);
     element.showModal();
-
-    // if(seatsBooked>0 && !isNaN(phoneNumber))
-    // {
-    //     hideEl('notPhone');
-    // }
-    // else{
-    //     (seatsBooked<=0)?unhideEl('lowSeat'):hideEl('lowSeat');
-
-    //     (isNaN(phoneNumber))?unhideEl('notPhone'):hideEl('notPhone');
-    // }
 }
 
-function activeNext()
-{
-    numberValue =parseInt(document.getElementById('phoneNumber').value);
+function activeNext() {
+    numberValue = parseInt(document.getElementById('phoneNumber').value);
 
-    if(selectedSeatNumber.length>0 && !isNaN(numberValue)){
+    if (selectedSeatNumber.length > 0 && !isNaN(numberValue)) {
         document.getElementById('nextButton').classList.remove('btn-disabled');
     }
-    else{
+    else {
         document.getElementById('nextButton').classList.add('btn-disabled');
 
     }
-    console.log('ACTIVE NEXT',document.getElementById('phoneNumber').value);
+    console.log('ACTIVE NEXT', document.getElementById('phoneNumber').value);
 }
 
-console.log('phone number',document.getElementById('phoneNumber').value);
+function applyCouponCode() {
+    var couponCodeValue = document.getElementById('userCouponCode').value;
+    var token = 0;
+    // console.log(couponCodeValue);
+    if (couponCodeValue === 'NEW15') {
+        var discount = ((15 * totalPrice) / 100);
+        token = 1;
+    }
+    else if(couponCodeValue === 'Couple 20')
+    {
+        var discount = ((20 * totalPrice) / 100);
+        token = 1;
+    } 
+    if(token == 1)
+    {
+        document.getElementById('discountPrice').innerText = discount;
+        document.getElementById('grandTotal').innerText = (totalPrice - discount);
+        document.getElementById('couponSection').classList.add('hidden');
+        document.getElementById('discountSection').classList.remove('hidden');
+        hideEl('couponCode');
+    }
+    else{
+        unhideEl('couponCode');
+    }
+
+    console.log(discount);
+
+}
